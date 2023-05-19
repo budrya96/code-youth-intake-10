8f8ba5ecc21258df5b72182507dc59dc0bb6da9a
budrya96 <131697011+budrya96@users.noreply.github.com>
2023-05-19 12:56:11 -0400

    Create loop.js

---
+++ M1 Fundamentals/javascript/loop.js
@@ -0,0 +1,46 @@
+//for(i=1;i<=20;i++){
+//   console.log("Budry")
+//    console.log("Ottawa")
+//}
+
+//for(i=0;i<=8;i++){
+//    console.log(i)
+//}
+
+//for(i=2;i<=20;i+=2){
+//    console.log(i)
+//}
+
+// for(i=19;i>=1;i-=2){
+//     console.log(i)
+// }
+
+// let sum = 0;
+// for(let i=1;i<=20;i++){
+//     sum = sum+i
+// }
+// console.log(sum/20);
+
+// for(i=1;i<=100;i++){
+//     if(i%2 == 0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log("odd")
+//     }
+// }
+
+for(i=1;i<101;i++){
+    if(i%3==0 && i%5==0){
+        console.log("3 5")
+    }
+    else if(i%5==0){
+        console.log("5")
+    }
+    else if(i%3==0){
+        console.log("3")
+    }
+    else{
+        console.log(i)
+    }
+}
