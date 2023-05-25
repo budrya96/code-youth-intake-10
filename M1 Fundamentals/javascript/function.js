1b37d1893612d6b947d7e64b35bf256ac40bc86a
budrya96 <131697011+budrya96@users.noreply.github.com>
2023-05-25 15:08:35 -0400

    Update function.js

--- M1 Fundamentals/javascript/function.js
+++ M1 Fundamentals/javascript/function.js
@@ -31,18 +31,51 @@
 // display(10);
 
 
-function display(){
-    let rand = Math.floor(Math.random()*10) //Randomly generates a number between 1 and 10
-    if(rand%2 == 0){
-        console.log(rand + ' is even')
+// function display(){
+//     let rand = Math.floor(Math.random()*10) //Randomly generates a number between 1 and 10
+//     if(rand%2 == 0){
+//         console.log(rand + ' is even')
+//     }
+//     else{
+//         console.log(rand + ' is odd')
+//     }
+// }
+// display();
+
+// function display(num1,num2,num3,num4){
+//     console.log(num1+num2+num3+num4)
+// }
+// display(10,3,23,94);
+
+ function check(num){
+     if(num>=0){
+         console.log("positive")
+     }
+     else if(num<0) {
+         console.log("negative")
     }
-    else{
-        console.log(rand + ' is odd')
-    }
-}
-display();
+        else{
+            console.log("Not a number")
+        }
+ }
+ check("o");
 
 
+// function display(num){
+//     console.log(num**3)
+//  }
+//  display(5);
+
+// function display(length, width){
+//     console.log(length*width)
+// }
+
+// display(7,4)
+
+// function display(){
+//     console.log(Math.random() * 100)
+// }
+// display();
 
     
 
